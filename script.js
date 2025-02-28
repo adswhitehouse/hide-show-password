4e118bd0d022c09c33720821636a41c89cb818a2
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-28 08:51:12 +0000

    shortened js code

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 let eyeIcon = document.querySelector(".jsEye");
 let passwordInput = document.querySelector("input");
 
-let isHidden = true;
 eyeIcon.addEventListener("click", () => {
-  if (isHidden) {
-    isHidden = false;
+  if (passwordInput.type === "password") {
     eyeIcon.classList.add("icon-red");
     eyeIcon.classList.add("fa-eye");
     passwordInput.type = "text";
   } else {
-    isHidden = true;
     eyeIcon.classList.remove("icon-red");
     eyeIcon.classList.remove("fa-eye");
     passwordInput.type = "password";
